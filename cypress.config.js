e5ce0da1c7f1578a203c6d8a5bd3c228eb29af50
Quoc Bao <baoquoc@smthgoodco.com>
2022-10-04 22:11:09 +0700

    Update config report

--- cypress.config.js
+++ cypress.config.js
@@ -25,24 +25,6 @@ module.exports = defineConfig({
       PASSWORD: "iRDb0vno",
     },
     defaultCommandTimeout: 60000,
-    setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-
-      on("task", {
-        deleteFolder(folderName) {
-          console.log("deleting folder %s", folderName);
-
-          return new Promise((resolve, reject) => {
-            rmdir(folderName, { maxRetries: 10, recursive: true }, (err) => {
-              if (err) {
-                console.error(err);
-                return reject(err);
-              }
-              resolve(null);
-            });
-          });
-        },
-      });
-    },
+    setupNodeEvents(on, config) {},
   },
 });
