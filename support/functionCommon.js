17b61825683414727b37a5e09d3d625e329366bb
SmthgoodCo <baoquoc@smthgoodco.com>
2022-10-06 11:04:37 +0700

    add file general common function

---
+++ support/functionCommon.js
@@ -0,0 +1,25 @@
+import { match } from "assert";
+
+export function getRandomEmail(){
+    return getRandomText() + getRandomNumber() + '@gmail.com';
+}
+
+export function getRandomText(){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (var i = 0; i < 10; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+export function getRandomNumber(){
+    var text = "";
+    var possible = "0123456789";
+
+    for (var i = 0; i < 10; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
