13988e9f934e886393649e2eb0b197daf4a48a12
SmthgoodCo <baoquoc@smthgoodco.com>
2022-10-28 22:39:11 +0700

    Update test case for edit profile

--- pages/editProfilePage.js
+++ pages/editProfilePage.js
@@ -13,7 +13,7 @@ export class EditProfilePage {
   }
 
   inputUsername(username = '') {
-    cy.wait(15000);
+    cy.wait(30000);
     cy.get("flt-glass-pane").click();
     cy.get("flt-glass-pane").shadow()
       .find('input.flt-text-editing').clear().type(username);
@@ -21,6 +21,7 @@ export class EditProfilePage {
   }
 
   verifyUsername(username = '') {
+    cy.wait(30000);
     cy.get("flt-glass-pane").click();
     cy.get("flt-glass-pane").shadow()
       .find('input.flt-text-editing').should('have.value', username);
@@ -28,20 +29,21 @@ export class EditProfilePage {
   }
 
   verifyErrorMsg(msg = '') {
-    cy.wait(15000);
+    cy.wait(30000);
     cy.get("flt-glass-pane").click();
     cy.get("flt-glass-pane").shadow()
       .find('flt-scene-host')
       .find('flt-transform')
-      .find('flt-clip-interior').contains(msg).should('be.visible');
+      .find('flt-clip-interior')
+      .find('flt-paragraph').contains(msg).should('be.visible')
     return this;
   }
 
   clickSaveButton() {
+    cy.wait(10000);
     cy.get("flt-glass-pane").shadow().within(() => {
       cy.get('flt-scene-host').find('flt-span').contains('SAVE').click({force: true});
     })
-    cy.wait(5000);
     return this;
   }
 }
